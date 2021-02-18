18e5e3301a4048863f2863edfedf5cc1ff959af6
masquee <okayanz@outlook.com>
2021-02-18 16:47:25 +0800

    add demo

---
+++ doc.sh
@@ -0,0 +1,11 @@
+
+# 使用 demo 配置
+envoy -c config/envoy-demo.yaml
+
+# 覆盖默认配置
+ envoy -c config/envoy-demo.yaml --config-yaml "$(cat config/envoy-override.yaml)"
+
+# 校验 envoy 配置文件
+ envoy --mode validate -c config/envoy-demo.yaml
+
+ 
